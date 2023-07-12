8e9aab83d7ab8ac20a23a9634cea4d3838d495be
afperdomo2 <alvarado0925@gmail.com>
2023-07-12 15:24:32 -0500

    Implementa Redis para la petición de Character de la API

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Redis con Node
 
+### Ejecutar el servidor de desarrollo
+
+```sh
+npm run dev
+```
+
 ### Crear una red
 
 ```sh
@@ -9,5 +15,21 @@ docker network create -d bridge redisnet
 ### Ejecutar Redis Container
 
 ```sh
+# Sin Red
+docker run -d -p 6379:6379 --name myredis redis
+
+# Con Red
 docker run -d -p 6379:6379 --name myredis --network redisnet redis
+```
+
+### Ejecutar Redis-Commander
+
+Este permite ver la base de datos de Redis
+
+```sh
+# Instalar el paquete global
+npm list -g redis-commander
+
+# Ejecutar en un puerto definido
+redis-commander --port 9000
 ```
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const axios = require('axios');
 const responseTime = require('response-time');
+const redis = require('redis');
+
+const PORT = 3900;
+const URL_API = 'https://rickandmortyapi.com/api/character';
 
 const app = express();
+const client = redis.createClient();
 
 app.use(responseTime());
 
 app.get('/character', async (req, res) => {
-  const response = await axios.get('https://rickandmortyapi.com/api/character');
-  res.json(response.data);
+  try {
+    // Si el registro existe en Redis, se devuelve
+    const getCharacters = await client.get('characters');
+    if (getCharacters) {
+      return res.json(JSON.parse(getCharacters));
+    }
+
+    // Si el registro no existe, se consulta en la API
+    const response = await axios.get(URL_API);
+    const characters = JSON.stringify(response.data);
+    // Se carga en Redis
+    await client.set('characters', characters);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-app.listen(3900);
-console.log('✅ Server on Port 3900');
+app.listen(PORT, async () => {
+  await client.connect();
+  console.log(`✅ Server on Port ${PORT}`);
+});
