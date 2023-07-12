61cc541bfd57df0e013b4ee421630cfd04ba950e
afperdomo2 <alvarado0925@gmail.com>
2023-07-12 09:03:13 -0500

    Implementa el Middleware de Response Time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const axios = require('axios');
+const responseTime = require('response-time');
 
 const app = express();
+
+app.use(responseTime());
 
 app.get('/character', async (req, res) => {
   const response = await axios.get('https://rickandmortyapi.com/api/character');
